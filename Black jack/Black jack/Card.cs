7013064f9a6c0e0bf89df72738df983cc9f07ccc
blockplocker <noahallklint@gmail.com>
2024-09-30 15:50:40 +0200

    Black jack start

---
+++ Black jack/Black jack/Card.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_jack
+{
+    class Card
+    {
+        public string Color { get; set; }
+        public string Number { get; set; }
+        public int Value { get; set; }
+
+        public Card(string color, string number)
+        {
+            this.Color = color;
+            this.Number = number;
+
+            int value;
+            if (int.TryParse(number, out value))
+            {
+                this.Value = value;
+            }
+            else
+            {
+                switch (number)
+                {
+                    case "J":
+                    case "Q":
+                    case "K":
+                        this.Value = 10;
+                        break;
+                    case "A":
+                        this.Value = 11;
+                        break;
+                    default:
+                        this.Value = 0;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Black jack/Black jack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace Black_jack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+
+
+            foreach (var card in deck.GetCards())
+            {
+                Console.WriteLine($"{card.Color} {card.Number} (Value: {card.Value})");
+            }
+
+            deck.Shuffle();
+
+            // Draw a single card
+            Card drawnCard = deck.DrawCard();
+            Console.WriteLine($"Drawn Card: {drawnCard.Color} {drawnCard.Number}");
+
+            //Card dragetKort = Deck.GetCard();
+            //Console.WriteLine("Du drog " + dragetKort.Number + " av " + dragetKort.Color);
+            //Console.WriteLine("Poängen för detta kort är: " + dragetKort.Value);
+        }
+    }
+}
