c6a3f88fbf35d9806b51362393623f08bece6f29
blockplocker <noahallklint@gmail.com>
2024-10-07 11:14:43 +0200

    hotell

---
+++ övningar/hotell/hotell/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Channels;
+
+namespace hotell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] hotell =
+            {
+                {"Tomt","Tomt","Tomt","Tomt"},
+                {"Tomt","Tomt","Tomt","Tomt"},
+                {"Tomt","Tomt","Tomt","Tomt"}
+            };
+
+            
+            while (true)
+            {
+                string myString = "";
+                string[] hotel = myString.Split('|');
+
+
+                Console.WriteLine("Våning 1: 650 kr/rum rum 1-4");
+                Console.WriteLine("Våning 2: 775 kr/rum rum 5-8");
+                Console.WriteLine("Våning 3: 995 kr/rum rum 9-12");
+
+                Console.WriteLine("vad heter du");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("vilket rum vill du bo i");
+                int.TryParse(Console.ReadLine(), out int bokning);
+
+                int rum = 1;
+                for (int row = 0; row < hotell.GetLength(0); row++)
+                {
+                    for (int col = 0; col < hotell.GetLength(1); col++)
+                    {
+                        rum = 4 * row + col + 1;
+                        if (rum == bokning)
+                        {
+                            hotell[row, col] = $"{rum}: {name}";
+                        }
+                        Console.Write($"{rum}: {hotell[row, col]} \t");
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ övningar/save to file/save to file/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace save_to_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
