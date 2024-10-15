0f34ecef183ae9a53f4442525dafa91e5ae56d1a
blockplocker <noahallklint@gmail.com>
2024-10-15 16:27:56 +0200

    Skogen Början

---
+++ Skogen/Skogen/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skogen
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+        public bool Nocturnal { get; set; }
+        public string  Movement { get; set; }
+
+        public Animal(string name, bool nocturnal, string movement)
+        {
+            Name = name;
+            Nocturnal = nocturnal;
+            Movement = movement;
+        }
+        
+
+
+    }
+}
---
+++ Skogen/Skogen/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Skogen
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Animal> forest = new List<Animal>();
+            forest.Add(new Animal("Varg", true, "springer"));
+            forest.Add(new Animal("Fladdermus", true, "flyger"));
+            forest.Add(new Animal("Älg", false, "springer"));
+            forest.Add(new Animal("Räv", true, "springer"));
+            forest.Add(new Animal("Uggla", true, "flyger"));
+            forest.Add(new Animal("Rådjur", false, "springer"));
+            forest.Add(new Animal("Ekorre", false, "springer"));
+            forest.Add(new Animal("Hare", false, "springer"));
+
+
+        }
+    }
+}
