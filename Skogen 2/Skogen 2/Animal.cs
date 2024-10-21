ceb5d6ff623b8c79f5fe27d6fe88d6f5687c16b6
blockplocker <noahallklint@gmail.com>
2024-10-21 11:16:03 +0200

    Skogen 2 Arv och override methoder

---
+++ Skogen 2/Skogen 2/Animal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skogen_2
+{
+    internal class Animal
+    {
+        public  int Weight { get; set; }
+        public bool Nocturnal { get; set; }
+
+        public Animal(int weight, bool nocturnal)
+        {
+            Weight = weight;
+            Nocturnal = nocturnal;
+        }
+
+        public virtual void Move(bool day)
+        {
+        }
+    }
+    class Owl : Animal
+    {
+        public int Wingspan { get; set; }
+        public Owl(int weight, bool nocturnal, int wingspan) : base(weight, nocturnal)
+        {
+            Wingspan = wingspan;
+        }
+        public override void Move(bool day)
+        {
+            if (Nocturnal && day == false)
+            {
+                Console.WriteLine("Ugglan flyger runt i skogen och letar efter mat");
+            }
+            else
+            {
+                Console.WriteLine("Ugglan är sover");
+            }
+        }
+    } 
+    class Horse : Animal
+    {
+        public int HayPerDay { get; set; }
+        public Horse(int weight, bool nocturnal, int hayPerDay) : base(weight, nocturnal)
+        {
+            HayPerDay = hayPerDay;
+        }
+        public override void Move(bool day)
+        {
+            if (Nocturnal == false && day == true)
+            {
+                Console.WriteLine("Hästen travar runt i skogen och äter hö");
+            }
+            else
+            {
+                Console.WriteLine("Hästen är sover");
+            }
+        }
+    }
+    class Dolfin : Animal
+    {
+        public int DistancePerDay { get; set; }
+        public Dolfin(int weight, bool nocturnal, int distancePerDay) : base(weight, nocturnal)
+        {
+            DistancePerDay = distancePerDay;
+        }
+        public override void Move(bool day)
+        {
+            if (Nocturnal == false && day == true)
+            {
+                Console.WriteLine("Delfinen simmar runt i sjön och letar efter fisk");
+            }
+            else
+            {
+                Console.WriteLine("Delfinen är sover");
+            }
+        }
+    }
+}
---
+++ Skogen 2/Skogen 2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Skogen_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Animal> forrest = new List<Animal>();
+            forrest.Add(new Owl(20, true, 185));
+            forrest.Add(new Horse(300, false, 10));
+            forrest.Add(new Dolfin(100, false, 100));
+
+            bool day = true;
+            while (true)
+            {
+                Console.Write("Dag(d) Natt(n): ");
+                string input = Console.ReadKey().KeyChar.ToString();
+                day = (input == "d" ? true : (input == "n" ? false : day));
+                Console.WriteLine();
+
+                foreach (Animal animal in forrest)
+                {
+                    Console.WriteLine(animal.GetType().Name + " " + animal.Weight + " kg " + (animal.Nocturnal ? " nattdjur " : "dagdjur"));
+                    if (animal is Owl)
+                    {
+                        Console.WriteLine("Ugglan har en vingbrädd på " + ((Owl)animal).Wingspan + " cm");
+                    }
+                    else if (animal is Horse)
+                    {
+                        Console.WriteLine("Hästen äter " + ((Horse)animal).HayPerDay + " kg hö per dag");
+                    }
+                    else if (animal is Dolfin)
+                    {
+                        Console.WriteLine("Dolfin simmar " + ((Dolfin)animal).DistancePerDay + " km per dag");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Okänt djur");
+                    }
+
+
+                    animal.Move(day);
+                }
+
+            }
+        }
+    }
+}
