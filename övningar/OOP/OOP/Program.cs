0e21ef766c30d8e0f19a6fbfa09a1e5d831c4b6b
blockplocker <noahallklint@gmail.com>
2024-11-06 15:32:55 +0100

    Diffrent list interfaces and threr benifits IEnumerable List

--- övningar/OOP/OOP/Program.cs
+++ övningar/OOP/OOP/Program.cs
@@ -13,6 +13,17 @@
             OOPD.Demo.Run();
             Console.WriteLine("------------------------------------");
             OOPE.Demo.Run();
+
+
+            List<int> intList = new List<int>() { 1, 2, 3, 4, 5 };
+            intList.Add(6);
+            int t = intList[1];
+
+            IEnumerable<int> intList2 = new List<int>() { 1, 2, 3, 4, 5 };   //IEnumerable är ett interface som inte har Add och indexering men tar mycket mindre minne
+            //intList2.Add(6);         lägga till finns inte i detta interface
+            //int t2 = intList2[1];    indexering finns inte i detta interface
+
+
         }
     }
 }
